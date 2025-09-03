db9cf0b9bbb7948ce5e1b0a1ea1bf3b81ecd31d8
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2025-09-03 19:48:09 +0200

    Update method name

--- 3-spring-webmvc-rest-services/my-fancy-pdf-invoices-spring-webmvc/src/main/java/io/github/ardavanghaffari/myfancypdfinvoices/web/GlobalExceptionHandler.java
+++ 3-spring-webmvc-rest-services/my-fancy-pdf-invoices-spring-webmvc/src/main/java/io/github/ardavanghaffari/myfancypdfinvoices/web/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handlemethodArgumentNotValid(MethodArgumentNotValidException exception) {
+    public String handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
         return "Sorry, that was not quite right: " + exception.getMessage();
     }
 
